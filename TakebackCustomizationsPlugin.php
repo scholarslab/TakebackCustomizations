1b67329ac1cab9dcd01be9621e6916800fee1941
Jeremy Boggs <jeremyboggs@gmail.com>
2016-08-30 15:05:31 -0400

    Simplifies conditional checks.

--- TakebackCustomizationsPlugin.php
+++ TakebackCustomizationsPlugin.php
@@ -11,30 +11,39 @@ class TakebackCustomizationsPlugin extends Omeka_Plugin_AbstractPlugin
      * Sets the default sort field to DC:Date, and the default order to ASC.
      */
     public function hookItemsBrowseSql($args) {
-        $select = $args['select'];
-        $params = $args['params'];
 
-        if ((!is_admin_theme()) && (!isset($_REQUEST['sort_field']))) {
-            $sort_field = 'Dublin Core,Date';
-            $sort_dir = 'ASC';
-            $select->reset('order');
-            get_db()->getTable('Item')->applySorting($select, $sort_field, $sort_dir);
+        if((is_admin_theme()) || (isset($_REQUEST['sort_field']))) {
+            return;
         }
+
+        $select = $args['select'];
+        $select->reset('order');
+
+        $sort_field = 'Dublin Core,Date';
+        $sort_dir = 'ASC';
+
+        get_db()->getTable('Item')->applySorting($select, $sort_field, $sort_dir);
+
     }
 
     /**
      * Order Collections
      */
     public function hookCollectionsBrowseSql($args) {
-        $select = $args['select'];
 
-        if (!isset($_REQUEST['sort_field'])) {
-            $select->reset(Zend_Db_Select::ORDER);
-            $sort_string = "CASE WHEN et_sort.text REGEXP '<[^>]*>' = 1 THEN TRIM(SUBSTR(et_sort.text, INSTR(et_sort.text ,' '))) ELSE et_sort.text END ASC";
-            $sort_order = new Zend_Db_Expr($sort_string);
-            $select->order($sort_order);
-            get_db()->getTable('Collection')->applySorting($select, 'Dublin Core,Title', 'ASC');
+        if((is_admin_theme()) || (isset($_REQUEST['sort_field']))) {
+            return;
         }
+
+        $select = $args['select'];
+        $select->reset(Zend_Db_Select::ORDER);
+
+        $sort_string = "CASE WHEN et_sort.text REGEXP '<[^>]*>' = 1 THEN TRIM(SUBSTR(et_sort.text, INSTR(et_sort.text ,' '))) ELSE et_sort.text END ASC";
+        $sort_order = new Zend_Db_Expr($sort_string);
+        $select->order($sort_order);
+
+        get_db()->getTable('Collection')->applySorting($select, 'Dublin Core,Title', 'ASC');
+
     }
 }
 
